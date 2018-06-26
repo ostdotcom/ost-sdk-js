998cc083d0e6ec68a8647f9b89a416d26db11995
santhu210 <santhosh.nitdgp2015@gmail.com>
2018-06-26 20:53:28 +0530

    Fix for failing execute transaction tests

--- test/v1_core/transactions/execute_arbitrary_commission_percent_and_amount.js
+++ test/v1_core/transactions/execute_arbitrary_commission_percent_and_amount.js
@@ -31,7 +31,7 @@ let startTests = function (  ) {
 
   it('FIRST PREPARE DATA FOR TRANSACTIONS', async function() {
     var actionData = {page_no: 1, limit: 100, order_by: 'created', order: 'desc', kind: 'user_to_user',
-      arbitrary_amount: true, arbitrary_commission: true};
+      arbitrary_amount: true, arbitrary_commission: true, currency: 'BT'};
     const response = await actionService.list(actionData).catch(function(e) {return e});
     transactionData.action_id = response.data.actions[0].id;
   });
