ba7fff75a1905ccd9207a6b5c9eb346769001bb5
Ashutosh <ashutosh@ost.com>
2018-06-26 16:53:15 +0530

    generic v1 and v1_1

--- test/v1_1/generic.js
+++ test/v1_1/generic.js
@@ -1,9 +1,9 @@
 // Load cache service
 const rootPrefix = "../.."
-  , helper = require(rootPrefix + '/test/v1/helper')
+  , helper = require(rootPrefix + '/test/v1_1/helper')
 ;
 
-describe('services/v1/generic', function () {
+describe('services/v1_1/generic', function () {
   const coreTestCases = require(rootPrefix + "/test/v1_core/generic");
   coreTestCases.setHelper( helper );
   coreTestCases.startTests();
